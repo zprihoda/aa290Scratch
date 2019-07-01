4f43fb0dd98ab7da66b00d1237db2df88cc11724
Zach <zach@laptop.stanford.edu>
2019-07-01 16:10:19 -0700

    Initial Commit of structuralsim

---
+++ structuralSim.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+## GLOBAL VARIABLES
+AXIAL_STIFFNESS = 8000*175.127	# convert to Newtons per meter
+LATERAL_STIFFNESS = 2*175.127
+
+AXIAL_STRENGTH = 90*4.44822	# Convert to Newtonts
+LATERAL_STRENGTH = 0.5*4.44822
+
+AXIAL_PRECISION = 0.002*0.0254	# m
+LATERAL_PRECISION = 0.05*0.0254
+
+SPACECRAFT_MASS = 20	# kg
+
+
+def main():
+	pass
+
+
+if __name__ == "__main__":
+	main()
