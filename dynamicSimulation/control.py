bc2b25cbe4497d687b0ac5325ac6e7481ccd41ff
Zach <zach@laptop.stanford.edu>
2019-07-19 16:41:36 -0700

    Add torsion damping controller

--- dynamicSimulation/control.py
+++ dynamicSimulation/control.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 def controlStep(arm, waypoint):
     """
@@ -8,12 +9,26 @@ def controlStep(arm, waypoint):
         u   : control vector
     """
 
+
     # damping controllers
-    # y_rot = [arm.state.rotz[0],arm.state.rotz[-1], arm.state.ratez[0], arm.state.ratez[-1]]       # we know the rotation angle and rate at the start and end of the boom
-    # u_torsion_damping = torsionDampingControl(y_rot)
+    y_rot = [arm.state.rot_z[0],arm.state.rot_z[-1], arm.state.rate_z[0], arm.state.rate_z[-1]]       # we know the rotation angle and rate at the start and end of the boom
+    u_torsion_damping = torsionDampingControl(y_rot)
     # TODO: include other controllers
     # TODO: build total u vector
 
-    u =  [0,0]
+    # u =  [0,0]
+    u = u_torsion_damping
+
     return u
 
+
+def torsionDampingControl(y):
+    g = 1e-3
+
+    w1 = y[2]
+    w2 = y[3]
+
+    u1 = -g*y[2]
+    u2 = 0
+    u = np.array([u1,u2])
+    return u
--- dynamicSimulation/simulate.py
+++ dynamicSimulation/simulate.py
@@ -28,14 +28,24 @@ def simulate(arm, traj, tf, dt, u_inj=None):
 
 def plotResults(state_list, t_arr):
 
-    rotz_arr = [state.rot_z for state in state_list]
-    rotz_arr = np.array(rotz_arr)
+    rot_z_arr = np.array([state.rot_z for state in state_list])
+    rate_z_arr = np.array([state.rate_z for state in state_list])
 
     n = state_list[0].n
+
+    fig,axes = pl.subplots(2,1,sharex=True)
     for i in range(n):
-        pl.plot(t_arr,rotz_arr[:,i])
-    pl.legend(range(n))
-    pl.title('Torsional Finite-Element Model')
+        axes[0].plot(t_arr, rot_z_arr[:,0])
+        axes[0].plot(t_arr, rot_z_arr[:,-1])
+        axes[1].plot(t_arr, rate_z_arr[:,0])
+        axes[1].plot(t_arr, rate_z_arr[:,-1])
+
+    axes[0].legend(['start','end'])
+    axes[0].set_title('Torsional Finite-Element Model')
+    axes[0].set_ylabel(r'$\theta$')
+    axes[1].set_ylabel(r'$\dot{\theta}$')
+    axes[1].set_xlabel('t')
+    pl.tight_layout()
     pl.show()
 
 if __name__ == "__main__":
@@ -52,7 +62,7 @@ if __name__ == "__main__":
     traj = np.zeros([2,len(t_arr)])
 
     u_inj = {}
-    u_inj[0] = np.array([1e-3, 0])
+    u_inj[0] = np.array([1e-2, 0])
     u_inj[1] = -u_inj[0]
 
     simulate(arm, traj, tf, dt, u_inj=u_inj)
