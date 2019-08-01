e814a368fcdfee2ff8b59e724b2f7fab99fb1688
Zach <zach@laptop.stanford.edu>
2019-08-01 15:52:12 -0700

    Cleanup controller script
    Assume we are commanding motor moments

--- dynamicSimulation/control.py
+++ dynamicSimulation/control.py
@@ -1,4 +1,5 @@
 import numpy as np
+import structuralProperties as structProp
 
 def controlStep(y, waypoint, mode):
     """
@@ -10,7 +11,6 @@ def controlStep(y, waypoint, mode):
         u   : control vector
     """
 
-
     # damping controllers
 
     # TODO: include other controllers
@@ -20,19 +20,33 @@ def controlStep(y, waypoint, mode):
         u =  [0,0]
     elif mode == 'damping':
         y_rot = y['rot']
-        u_torsion_damping = torsionDampingControl(y_rot)
-        u = u_torsion_damping
+
+        r = y['r']
+        I = structProp.getBoomInertia(r)
+
+        ddtheta = torsionDampingControl(y_rot)
+        M = I*ddtheta
+
+        u = M   # assume we are commanding the moment
 
     return u
 
 
 def torsionDampingControl(y):
+    """
+    Torsional Damping Controller
+
+    Inputs:
+        y: measurements [theta_start, theta_end, w_start, w_end]
+    Outputs:
+        u: ddtheta (d^2(theta)/dt^2
+    """
     g = 1e-3
 
     w1 = y[2]
     w2 = y[3]
 
-    u1 = -g*y[2]
-    u2 = 0
+    u1 = 0
+    u2 = -g*(w2-w1)
     u = np.array([u1,u2])
     return u
