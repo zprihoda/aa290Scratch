a98a2bb30e2565c7300817302af8a00a805ff020
Zach <zach@laptop.stanford.edu>
2019-08-01 15:53:23 -0700

    Assume we measure the arm length

--- dynamicSimulation/measurement.py
+++ dynamicSimulation/measurement.py
@@ -11,6 +11,6 @@ def simulateMeasurements(arm):
 
     y['rot'] = [arm.state.rot_z[0],arm.state.rot_z[-1],
                 arm.state.rate_z[0], arm.state.rate_z[-1]]     # we know the rotation angle and rate at the start and end of the boom
-
+    y['r'] = arm.r
 
     return y
