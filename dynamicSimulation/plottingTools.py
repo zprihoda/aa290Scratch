1a41edc3534ad9456bfd1f14e001e14fdb299e88
zprihoda <zach@laptop>
2019-09-23 21:48:22 -0700

    Fixup plots

--- dynamicSimulation/plottingTools.py
+++ dynamicSimulation/plottingTools.py
@@ -4,15 +4,41 @@ import numpy as np
 
 
 def plotResults(X_arr, u_arr, t_arr):
-    # plot hub angle vs time
+
+    # obtain x,y,theta coordinates from state
+    n = (len(X_arr[:,0])-2)//4
+    v_arr = X_arr[1:2*n:2,:]   # deflection at each element at each time
+    theta_arr = X_arr[0,:]  # hub angle at each time
+    u = np.linspace(0, 2, n+1)
+
+    x_arr = np.zeros([len(t_arr)])
+    y_arr = np.zeros([len(t_arr)])
+    for i in range(len(t_arr)):
+        v = np.hstack([0,v_arr[:,i]])
+        theta = theta_arr[i]
+
+        R = np.array([[np.cos(theta), -np.sin(theta)],[np.sin(theta),np.cos(theta)]])
+        tmp = np.vstack([u,v])
+        tmp2 = np.dot(R,tmp)
+        x = tmp2[0,:]
+        y = tmp2[1,:]
+
+        x_arr[i] = x[-1]
+        y_arr[i] = y[-1]
+
+    # plot x,y vs time
     plt.figure()
-    plt.plot(t_arr,np.rad2deg(X_arr[0,:-1]))
-    plt.title('Hub Angle vs Time')
-    plt.xlabel('t')
-    plt.ylabel(r'$\theta$')
+    plt.plot(t_arr,x_arr,'b-')
+    plt.plot(t_arr,y_arr,'o-')
+    plt.plot([t_arr[0],t_arr[-1]], [0,0],'b--')
+    plt.plot([t_arr[0],t_arr[-1]], [2,2],'o--')
+    plt.title('Payload Position vs Time')
+    plt.xlabel('t (s)')
+    plt.ylabel('position (m)')
+    plt.legend(['x','y'])
     plt.grid()
 
-    plt.savefig('plots/HubAngle.png')
+    plt.savefig('plots/position.png')
     plt.close()
 
     # plot deflection of end point vs time
@@ -20,8 +46,8 @@ def plotResults(X_arr, u_arr, t_arr):
     plt.figure()
     plt.title('Endpoint Deflection vs Time')
     plt.plot(t_arr,X_arr[n//2-1,:-1])
-    plt.xlabel('t')
-    plt.ylabel('v')
+    plt.xlabel('t (s)')
+    plt.ylabel('v (m)')
     plt.grid()
 
     plt.savefig('plots/EndPointDef.png')
@@ -33,8 +59,8 @@ def plotResults(X_arr, u_arr, t_arr):
     plt.plot(t_arr,u_arr[0,:])
     plt.plot(t_arr,u_arr[1,:])
     plt.legend(['Start','end'])
-    plt.xlabel('t')
-    plt.ylabel('u')
+    plt.xlabel('t (s)')
+    plt.ylabel('u (Nm)')
     plt.grid()
 
     plt.savefig('plots/Control.png')
@@ -48,7 +74,7 @@ def animateResults(X_arr, t_arr):
     n = (len(X_arr[:,0])-2)//4
     v_arr = X_arr[1:2*n:2,:]   # deflection at each element at each time
     theta_arr = X_arr[0,:]  # hub angle at each time
-    u = np.linspace(0, 1, n+1)
+    u = np.linspace(0, 2, n+1)
 
     x_arr = np.zeros([len(u),len(t_arr)])
     y_arr = np.zeros([len(u),len(t_arr)])
@@ -82,9 +108,9 @@ def animateResults(X_arr, t_arr):
     # initialize plot
     line = ax.plot([],[],'b.-')[0]
     ax.set(xlim=[c-d, c+d],ylim=[c-d, c+d])
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_title("Arm State")
+    ax.set_xlabel('x (m)')
+    ax.set_ylabel('y (m)')
+    ax.set_title("Setpoint Maneuver")
 
     def animate(i):
         x = x_arr[:,i]
@@ -95,7 +121,7 @@ def animateResults(X_arr, t_arr):
     # generate animation
     anim = ani.FuncAnimation(fig, animate, frames=np.arange(0,len(t_arr),2), interval=20)
     anim.save("plots/full.mp4")
-    # plt.show()
+    plt.close()
 
 def animateBending(X_arr, t_arr):
 
@@ -103,7 +129,7 @@ def animateBending(X_arr, t_arr):
     n = (len(X_arr[:,0])-2)//4
     v_arr = X_arr[1:2*n:2,:]   # deflection at each element at each time
     theta_arr = X_arr[0,:]  # hub angle at each time
-    u = np.linspace(0, 1, n+1)
+    u = np.linspace(0, 2, n+1)
 
     y_arr = np.zeros([len(u),len(t_arr)])
     for i in range(len(t_arr)):
@@ -121,9 +147,9 @@ def animateBending(X_arr, t_arr):
 
     # initialize plot
     line = ax.plot([],[],'b.-')[0]
-    ax.set(xlim=[0, 1],ylim=[c-d, c+d])
-    ax.set_xlabel('u')
-    ax.set_ylabel('v')
+    ax.set(xlim=[0, max(u)],ylim=[c-d, c+d])
+    ax.set_xlabel('u (m)')
+    ax.set_ylabel('v (m)')
     ax.set_title("Arm Deflection")
 
     def animate(i):
@@ -135,3 +161,4 @@ def animateBending(X_arr, t_arr):
     # generate animation
     anim = ani.FuncAnimation(fig, animate, frames=np.arange(0,len(t_arr),2), interval=20)
     anim.save("plots/bending.mp4")
+    plt.close()
